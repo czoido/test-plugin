163d207c5dd291c38669f3b7c45079174ea20723
GitHub Action <action@github.com>
2024-09-30 05:40:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/czoido/testplugin/MyBundle.kt
+++ src/main/kotlin/com/github/czoido/testplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.czoido.testplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/czoido/testplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/czoido/testplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.czoido.testplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
